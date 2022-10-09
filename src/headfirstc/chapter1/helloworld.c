be71b4205f2d0631adb054e841c423f3fec7d565
kosi <kosisochukwu.asuzu@e4email.net>
2022-10-09 09:03:28 +0100

    feat: set up c example project

---
+++ src/headfirstc/chapter1/helloworld.c
@@ -0,0 +1,20 @@
+/* 
+* This is a program for calculating the number of cards available given a number of decks
+*/
+
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int decks;
+    puts("Enter the number of decks");
+    scanf("%i", &decks);
+
+    if(decks < 1){
+        puts("Invalid number of decks");
+        return 1;
+    }
+
+    printf("The number of cards in the deck(s) are %i \n", decks);
+    return 0;
+}
