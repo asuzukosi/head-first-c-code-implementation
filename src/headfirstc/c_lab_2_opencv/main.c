b82f12932de933c9b8943c1ed0ff12584242f969
kosi <kosisochukwu.asuzu@e4email.net>
2022-11-10 16:28:34 +0100

    started implementing the opencv task but could not finish because opencv only supports cpp from version 4

--- src/headfirstc/c_lab_2_opencv/main.c
+++ src/headfirstc/c_lab_2_opencv/main.c
@@ -2,3 +2,10 @@
 This is a sample project that makes use of open cv 
 for intruder detection on your computer
 */
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    Mat image ;
+    return 0;
+}
